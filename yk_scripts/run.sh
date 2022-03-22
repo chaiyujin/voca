0e1fc61eb5f7d046e58738a3baa04df0c18572dd
yuki-chai <chaiyujin@gmail.com>
2022-03-22 18:15:14 +0800

    Update run.sh

--- yk_scripts/run.sh
+++ yk_scripts/run.sh
@@ -2,8 +2,11 @@ LIB=yk_scripts/functions.sh
 source ${LIB}
 source yk_scripts/nohup_run.sh
 
-# * Keep avoffset unchanged
-# RUN_YK_EXP --data_src=celebtalk --speaker=m001_trump --epoch=50 --load_step=100400 "$@";
+# > Keep avoffset unchanged
+# RUN_YK_EXP --data_src=celebtalk --speaker=m001_trump   --epoch=50 --load_step=100400 "$@";
 
-# * Manually correct avoffset
-# RUN_YK_EXP --data_src=celebtalk --speaker=m001_trump --epoch=50 --avoffset_ms=33.333333333333336 --load_step=98200 "$@";
+# > Manually correct avoffset
+# RUN_YK_EXP --data_src=celebtalk --speaker=m001_trump   --avoffset_ms=33.333333333333336 --epoch=50 --load_step=98200 "$@";
+# RUN_YK_EXP --data_src=celebtalk --speaker=m000_obama   --avoffset_ms=100.0              --epoch=50 --load_step=97250 "$@";
+# RUN_YK_EXP --data_src=celebtalk --speaker=f000_watson  --avoffset_ms=133.33333333333334 --epoch=50 --load_step=95900 "$@";
+# RUN_YK_EXP --data_src=celebtalk --speaker=f001_clinton --avoffset_ms=100.0              --epoch=50 --load_step=98500 "$@";
